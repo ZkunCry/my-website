821290f7894addf387f0393f99e7e73d3cd2c4b6
ZkunCry <aksentev.03@mail.ru>
2024-09-29 00:35:17 +0500

    init commit

--- src/components/TypingText/TypingText.tsx
+++
@@ -1,7 +0,0 @@
-// import "./TypingText.css"; // Импортируем CSS файл
-
-// const TypingText = ({ children }) => {
-//   return <div className="typing ">{children}</div>;
-// };
-
-// export default TypingText;
--- src/utils/variants.ts
+++
@@ -1,4 +0,0 @@
-export const variantsButton = {
-  primary:
-    "px-[10px] text-white text-[1.5rem] rounded-[10px] bg-primary-button text-center leading-[3rem]",
-};
