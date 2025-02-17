21e828ba67e12023182358b081240762e6d8f1db
Eugene Aksentev <aksentev.03@mail.ru>
2025-02-17 11:54:20 +0500

    Updates

--- components/CarouselSlider/CarouselSliderContext/Context.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-type CarouseSlideContextType = {
-  widthCard: string;
-  heightCard: string;
-};
-export const CarouselSlideContext = createContext<CarouseSlideContextType>({
-  widthCard: "",
-  heightCard: "",
-});
--- components/ProjectCard/ProjectCard.tsx
+++
@@ -1,7 +0,0 @@
-import cn from "../../utils/cn";
-
-export const ProjectCard = ({ children, className }) => (
-  <div className={cn("project-card bg-zinc-800 rounded-3xl p-8", className)}>
-    {children}
-  </div>
-);
